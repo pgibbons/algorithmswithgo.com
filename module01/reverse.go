026c2e76908af644535d5623f85b96bb7a7da287
pgibbons <6475190+pgibbons@users.noreply.github.com>
2022-04-10 10:51:13 -0700

    Convert to Rune to allow other languages

--- module01/reverse.go
+++ module01/reverse.go
@@ -9,16 +9,11 @@ import "strings"
 //   Reverse("alphabet") => "tebahpla"
 //
 func Reverse(word string) string {
-	// return empty case
-	if len(word) == 0 {
-		return ""
-	}
-
 	// reverse order building
 	var result_builder strings.Builder
-	for i := (len(word) - 1); i >= 0; i-- {
-		result_builder.WriteByte(word[i])
+	runes := []rune(word)
+	for i := (len(runes) - 1); i >= 0; i-- {
+		result_builder.WriteRune(runes[i])
 	}
-
 	return result_builder.String()
 }
