11acdb0eb7eb2c0f192a206f16bf172ac8eddc55
pgibbons <6475190+pgibbons@users.noreply.github.com>
2022-04-10 10:20:15 -0700

    Implement sum array

--- module01/sum.go
+++ module01/sum.go
@@ -3,5 +3,11 @@ package module01
 // Sum will sum up all of the numbers passed
 // in and return the result
 func Sum(numbers []int) int {
-	return 0
+	results := 0
+	// loop and find the number
+	for _, value := range numbers {
+		results += value
+	}
+	// return our results
+	return results
 }
